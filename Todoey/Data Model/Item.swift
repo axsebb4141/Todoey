41539553afe7a1ae105bb4dfaab2bb1373f18833
Andrew Sebben <andrewsebben4141@gmail.com>
2019-06-03 20:27:21 -0500

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Andrew Sebben on 4/3/19.
+//  Copyright © 2019 Andrew Sebben. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
